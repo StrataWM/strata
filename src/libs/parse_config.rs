ad1f16193e7b6d2dff4562159ea4a885fae5d909
Anant Narayan <anantnrg@gmail.com>
2023-07-15 13:42:47 +0530

    :twisted_rightwards_arrows: Simplified the code to get the config file path and removed unnecessary crates

--- src/libs/parse_config.rs
+++ src/libs/parse_config.rs
@@ -1,7 +1,7 @@
 use crate::libs::structs::Keybind;
 use colored::Colorize;
-use dirs::config_dir;
 use std::{
+	env::var,
 	fs::File,
 	io::Read,
 };
@@ -9,45 +9,23 @@ use toml::Value;
 
 impl Keybind {
 	fn new(key: &str, cmd: &str) -> Self {
-		Self {
-			key: key.to_string(),
-			cmd: cmd.to_string(),
-		}
+		Self { key: key.to_string(), cmd: cmd.to_string() }
 	}
 }
 
 pub fn parse_config() -> Vec<Keybind> {
-	let conf_path = match config_dir() {
-		Some(mut path) => {
-			path.push("strata");
-			path
-		}
-		None => {
-			println!(
-				"{}",
-				"Cannot access Strata config directory at ~/.config/strata\nCheck that your \
-				 $XDG_CONFIG_DIR is set and the folder exists\nStrata will run normally but you \
-				 are adviced to create the config directory"
-					.red()
-					.bold()
-			);
-			return vec![];
-		}
-	};
-
-	let conf_file_path = format!("{}{}", conf_path.display(), "/strata.toml");
+	let conf_file_path = format!(
+		"{}/.config/strata/strata.toml",
+		var("HOME").expect("This variable should be set!!!")
+	);
 	println!("{}", conf_file_path);
 
 	let mut conf_file =
 		File::open(conf_file_path).expect("Couldn't open config file. Check if it exits");
 	let mut conf_str = String::new();
-	conf_file
-		.read_to_string(&mut conf_str)
-		.expect("Couldn't read config!");
+	conf_file.read_to_string(&mut conf_str).expect("Couldn't read config!");
 
-	let config: Value = conf_str
-		.parse()
-		.expect("Couldn't parse TOML file. Check syntax");
+	let config: Value = conf_str.parse().expect("Couldn't parse TOML file. Check syntax");
 
 	let keybinds = config["keybinds"]["bind"].as_array().unwrap();
 
@@ -58,5 +36,6 @@ pub fn parse_config() -> Vec<Keybind> {
 		let cmd = bind["command"].as_str().unwrap();
 		return_keybinds.push(Keybind::new(key, cmd));
 	}
+	println!("{:?}", return_keybinds);
 	return return_keybinds;
 }
