2b5d2764566d1a1c4f0c423b79dae7bd7bd3eb0d
Anant Narayan <anantnrg@gmail.com>
2023-08-20 12:17:13 +0530

    Removed stale code

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	let lib_dir = xdg.find_data_file("lua");
 	let log_dir = xdg.get_state_home();
 
-	println!("{:?}", lib_dir);
-
 	if let (Some(config_path), Some(data_path)) = (config_dir, lib_dir) {
 		tokio::spawn(async { parse_config(config_path, data_path) }).await??;
 	}
