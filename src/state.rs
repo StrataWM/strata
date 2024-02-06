3e0e65e5f6dfe724a688c6c87643960ecb8f20a4
Anant Narayan <anantnrg@gmail.com>
2024-02-06 11:08:04 +0530

    Start working on Udev

--- src/state.rs
+++ src/state.rs
@@ -17,12 +17,35 @@ use std::{
 
 use piccolo as lua;
 use smithay::{
-	backend::input::{
-		Event,
-		InputBackend,
-		InputEvent,
-		KeyState,
-		KeyboardKeyEvent,
+	backend::{
+		allocator::{
+			dmabuf::{
+				AnyError,
+				Dmabuf,
+			},
+			Allocator,
+		},
+		drm::DrmNode,
+		input::{
+			Event,
+			InputBackend,
+			InputEvent,
+			KeyState,
+			KeyboardKeyEvent,
+		},
+		renderer::{
+			damage::OutputDamageTracker,
+			element::texture::TextureBuffer,
+			gles::GlesRenderer,
+			glow::GlowRenderer,
+			multigpu::{
+				gbm::GbmGlesBackend,
+				GpuManager,
+				MultiTexture,
+			},
+		},
+		session::libseat::LibSeatSession,
+		winit::WinitGraphicsBackend,
 	},
 	desktop::{
 		layer_map_for_output,
@@ -71,6 +94,10 @@ use smithay::{
 		compositor::{
 			CompositorClientState,
 			CompositorState,
+		},
+		dmabuf::{
+			DmabufGlobal,
+			DmabufState,
 		},
 		output::OutputManagerState,
 		selection::{
@@ -239,6 +266,29 @@ impl StrataState {
 
 		Ok(())
 	}
+}
+
+pub struct WinitData {
+	backend: WinitGraphicsBackend<GlowRenderer>,
+	damage_tracker: OutputDamageTracker,
+}
+
+pub struct UdevData {
+	pub session: LibSeatSession,
+	dh: DisplayHandle,
+	dmabuf_state: Option<(DmabufState, DmabufGlobal)>,
+	primary_gpu: DrmNode,
+	allocator: Option<Box<dyn Allocator<Buffer = Dmabuf, Error = AnyError>>>,
+	gpus: GpuManager<GbmGlesBackend<GlesRenderer>>,
+	backends: HashMap<DrmNode, BackendData>,
+	pointer_images: Vec<(xcursor::parser::Image, TextureBuffer<MultiTexture>)>,
+	pointer_element: PointerElement<MultiTexture>,
+	pointer_image: crate::cursor::Cursor,
+}
+
+pub enum Backend {
+	Winit(WinitData),
+	Udev(UdevData),
 }
 
 pub struct StrataComp {
