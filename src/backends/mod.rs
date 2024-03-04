332c4026732e47a3fc9568c116d952423a25fdfc
Anant Narayan <anantnrg@gmail.com>
2024-03-04 20:06:12 +0530

    start working on udev backend

--- src/backends/mod.rs
+++ src/backends/mod.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 pub mod cursor;
+pub mod udev;
 use log::error;
 
 use crate::backends::{
--- src/backends/udev.rs
+++ src/backends/udev.rs
@@ -1,3 +1,15 @@
+pub struct UdevData {
+	pub session: LibSeatSession,
+	dh: DisplayHandle,
+	dmabuf_state: Option<(DmabufState, DmabufGlobal)>,
+	primary_gpu: DrmNode,
+	allocator: Option<Box<dyn Allocator<Buffer = Dmabuf, Error = AnyError>>>,
+	gpus: GpuManager<GbmGlesBackend<GlesRenderer>>,
+	backends: HashMap<DrmNode, BackendData>,
+	pointer_images: Vec<(xcursor::parser::Image, TextureBuffer<MultiTexture>)>,
+	pointer_element: PointerElement<MultiTexture>,
+	pointer_image: crate::backends::cursor::Cursor,
+}
 use std::collections::HashMap;
 
 use smithay::{
