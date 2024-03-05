2581573194889a2deea0f37e3d028e52a0d77354
Anant Narayan <anantnrg@gmail.com>
2024-03-05 22:03:18 +0530

    More stuff!
    Signed-off-by: Anant Narayan <anantnrg@gmail.com>

--- src/backends/drawing.rs
+++ src/backends/drawing.rs
@@ -6,12 +6,21 @@ use smithay::{
 				MemoryRenderBufferRenderElement,
 			},
 			surface::WaylandSurfaceRenderElement,
+			AsRenderElements,
+			Kind,
 		},
 		ImportAll,
 		ImportMem,
+		Renderer,
+		Texture,
 	},
 	input::pointer::CursorImageStatus,
 	render_elements,
+	utils::{
+		Physical,
+		Point,
+		Scale,
+	},
 };
 
 pub struct PointerElement {
@@ -39,4 +48,56 @@ render_elements! {
 	pub PointerRenderElement<R> where R: ImportAll + ImportMem;
 	Surface=WaylandSurfaceRenderElement<R>,
 	Memory=MemoryRenderBufferRenderElement<R>,
+}
+
+impl<T: Texture + 'static, R> AsRenderElements<R> for PointerElement
+where
+	R: Renderer<TextureId = T> + ImportAll + ImportMem,
+{
+	type RenderElement = PointerRenderElement<R>;
+	fn render_elements<E>(
+		&self,
+		renderer: &mut R,
+		location: Point<i32, Physical>,
+		scale: Scale<f64>,
+		alpha: f32,
+	) -> Vec<E>
+	where
+		E: From<PointerRenderElement<R>>,
+	{
+		match &self.status {
+			CursorImageStatus::Hidden => vec![],
+			CursorImageStatus::Named(_) => {
+				if let Some(buffer) = self.buffer.as_ref() {
+					vec![PointerRenderElement::<R>::from(
+						MemoryRenderBufferRenderElement::from_buffer(
+							renderer,
+							location.to_f64(),
+							buffer,
+							None,
+							None,
+							None,
+							Kind::Cursor,
+						)
+						.expect("Lost system pointer buffer"),
+					)
+					.into()]
+				} else {
+					vec![]
+				}
+			}
+			CursorImageStatus::Surface(surface) => {
+				let elements: Vec<PointerRenderElement<R>> =
+					smithay::backend::renderer::element::surface::render_elements_from_surface_tree(
+						renderer,
+						surface,
+						location,
+						scale,
+						alpha,
+						Kind::Cursor,
+					);
+				elements.into_iter().map(E::from).collect()
+			}
+		}
+	}
 }
