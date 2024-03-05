feb2216306b2f0bd64b3131fbb2397061e23b5ea
Anant Narayan <anantnrg@gmail.com>
2024-03-05 21:58:16 +0530

    Implement render_elements! for PointerElement
    Signed-off-by: Anant Narayan <anantnrg@gmail.com>

--- src/backends/drawing.rs
+++ src/backends/drawing.rs
@@ -1,15 +1,42 @@
 use smithay::{
 	backend::renderer::{
 		element::{
-			memory::MemoryRenderBuffer,
-			AsRenderElements,
+			memory::{
+				MemoryRenderBuffer,
+				MemoryRenderBufferRenderElement,
+			},
+			surface::WaylandSurfaceRenderElement,
 		},
-		Renderer,
+		ImportAll,
+		ImportMem,
 	},
 	input::pointer::CursorImageStatus,
+	render_elements,
 };
 
 pub struct PointerElement {
 	buffer: Option<MemoryRenderBuffer>,
 	status: CursorImageStatus,
+}
+
+impl Default for PointerElement {
+	fn default() -> Self {
+		Self { buffer: Default::default(), status: CursorImageStatus::default_named() }
+	}
+}
+
+impl PointerElement {
+	pub fn set_status(&mut self, status: CursorImageStatus) {
+		self.status = status;
+	}
+
+	pub fn set_buffer(&mut self, buffer: MemoryRenderBuffer) {
+		self.buffer = Some(buffer);
+	}
+}
+
+render_elements! {
+	pub PointerRenderElement<R> where R: ImportAll + ImportMem;
+	Surface=WaylandSurfaceRenderElement<R>,
+	Memory=MemoryRenderBufferRenderElement<R>,
 }
